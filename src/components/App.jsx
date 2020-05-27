7fe4716ecc8742df85fdc76b09636b8b65c59573
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-05-27 11:40:54 -0700

    completed videoList & VideoListEntry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 
+//import
 import exampleVideoData from '../data/exampleVideoData.js';
+import Search from './Search.js';
 import VideoPlayer from './VideoPlayer.js';
 import VideoList from './VideoList.js';
 
@@ -7,17 +9,17 @@ var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5>
-        </div>
+        {/* //functional -rendering */}
+        <Search />
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em><VideoPlayer video={exampleVideoData[0]} /></h5></div>
+        <VideoPlayer />
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> <VideoList videos={exampleVideoData} /></h5>
-        </div>
+        {/* props <= videos */}
+        <VideoList videos={exampleVideoData}/>
       </div>
     </div>
   </div>
@@ -26,6 +28,38 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
+
+
+// //import
+// import exampleVideoData from '../data/exampleVideoData.js';
+// import Search from './Search.js';
+// import VideoPlayer from './VideoPlayer.js';
+// import VideoList from './VideoList.js';
+
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em> view goes here</h5>
+//         </div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <div><h5><em>videoPlayer</em><VideoPlayer video={exampleVideoData[0]} /></h5></div>
+//       </div>
+//       <div className="col-md-5">
+//         <div><h5><em>videoList</em> <VideoList videos={exampleVideoData} /></h5>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// );
+
+// // In the ES6 spec, files are "modules" and do not share a top-level scope
+// // `var` declarations will only exist globally where explicitly defined
+// export default App;
 
 
 //=> move it to index.js
