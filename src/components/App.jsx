3b34cb43b497eda511c7637fa35e0e905ae6f439
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-05-27 01:14:30 -0700

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-var App = (props) => (
+
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoPlayer from './VideoPlayer.js';
+import VideoList from './VideoList.js';
+
+var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <div><h5><em>search</em> view goes here</h5>
+        </div>
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>videoPlayer</em><VideoPlayer video={exampleVideoData[0]} /></h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em>videoList</em> <VideoList videos={exampleVideoData} /></h5>
+        </div>
       </div>
     </div>
   </div>
