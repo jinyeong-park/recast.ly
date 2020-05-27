ccfe97235d0803f54ae8119b32f93c693c156584
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-05-27 12:48:45 -0700

    show player(first)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <VideoPlayer />
+        <VideoPlayer video={exampleVideoData[0]} />
       </div>
       <div className="col-md-5">
         {/* props <= videos */}
@@ -29,37 +29,6 @@ var App = () => (
 // `var` declarations will only exist globally where explicitly defined
 export default App;
 
-
-
-// //import
-// import exampleVideoData from '../data/exampleVideoData.js';
-// import Search from './Search.js';
-// import VideoPlayer from './VideoPlayer.js';
-// import VideoList from './VideoList.js';
-
-// var App = () => (
-//   <div>
-//     <nav className="navbar">
-//       <div className="col-md-6 offset-md-3">
-//         <div><h5><em>search</em> view goes here</h5>
-//         </div>
-//       </div>
-//     </nav>
-//     <div className="row">
-//       <div className="col-md-7">
-//         <div><h5><em>videoPlayer</em><VideoPlayer video={exampleVideoData[0]} /></h5></div>
-//       </div>
-//       <div className="col-md-5">
-//         <div><h5><em>videoList</em> <VideoList videos={exampleVideoData} /></h5>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// );
-
-// // In the ES6 spec, files are "modules" and do not share a top-level scope
-// // `var` declarations will only exist globally where explicitly defined
-// export default App;
 
 
 //=> move it to index.js
